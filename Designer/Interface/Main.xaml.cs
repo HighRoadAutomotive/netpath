d51c51b056a6ef5c4ad9cc376e99308958738bc3
Adam Wilson <adamw@prospectivesoftware.com>
2012-08-04 19:48:46 -0700

    Set up the Solution Configurations. Options screen now works.

--- Designer/Interface/Main.xaml.cs
+++ Designer/Interface/Main.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace WCFArchitect.Interface
 {
@@ -30,6 +31,13 @@ namespace WCFArchitect.Interface
 		public ObservableCollection<Button> MessageActions { get { return (ObservableCollection<Button>)GetValue(MessageActionsProperty); } set { SetValue(MessageActionsProperty, value); } }
 		public static readonly DependencyProperty MessageActionsProperty = DependencyProperty.Register("MessageActions", typeof(ObservableCollection<Button>), typeof(Main));
 
+		//Options Properties
+		public Prospective.Server.Licensing.LicenseInfoWPF LicenseInfo { get { return (Prospective.Server.Licensing.LicenseInfoWPF)GetValue(LicenseInfoProperty); } set { SetValue(LicenseInfoProperty, value); } }
+		public static readonly DependencyProperty LicenseInfoProperty = DependencyProperty.Register("LicenseInfo", typeof(Prospective.Server.Licensing.LicenseInfoWPF), typeof(Main));
+
+		public WCFArchitect.Options.UserProfile UserProfile { get { return (WCFArchitect.Options.UserProfile)GetValue(UserProfileProperty); } set { SetValue(UserProfileProperty, value); } }
+		public static readonly DependencyProperty UserProfileProperty = DependencyProperty.Register("UserProfile", typeof(WCFArchitect.Options.UserProfile), typeof(Main));
+
 		public bool IsProcessingMessage { get; private set; }
 
 		public Main()
@@ -39,9 +47,39 @@ namespace WCFArchitect.Interface
 			if (Environment.OSVersion.Version.Major == 6 && Environment.OSVersion.Version.Minor == 2) Globals.WindowsLevel = Globals.WindowsVersion.WinEight;
 
 			MessageActions = new ObservableCollection<Button>();
-
+			
 			InitializeComponent();
+
+			//Initialize the Options screen.
+			UserProfile = Globals.UserProfile;
+			AboutVersion.Content = "Version " + System.Diagnostics.FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).FileVersion;
+#if TRIAL
+			ProductTitle.Content = "Prospective Software WCF Architect Trial Edition";
+#elif WINRT
+			ProductTitle.Content = "Prospective Software WCF Architect for Windows Runtime";
+#elif PROFESSIONAL
+			ProductTitle.Content = "Prospective Software WCF Architect Professional";
+#else
+			ProductTitle.Content = "Prospective Software WCF Architect Developer";
+#endif
+
+			//Set the logo in the options screen.
+			BitmapImage logo = new BitmapImage();
+			logo.BeginInit();
+#if TRIAL
+			logo.UriSource = new Uri("pack://application:,,,/WCFArchitect;component/Icons/Odd/FullLogoTrial.png");
+#elif WINRT
+			logo.UriSource = new Uri("pack://application:,,,/WCFArchitect;component/Icons/Odd/FullLogoWinRT.png");
+#elif PROFESSIONAL
+			logo.UriSource = new Uri("pack://application:,,,/WCFArchitect;component/Icons/Odd/FullLogoProfessional.png");
+#else
+			logo.UriSource = new Uri("pack://application:,,,/WCFArchitect;component/Icons/Odd/FullLogoDeveloper.png");
+#endif
+			logo.EndInit();
+			SKULevel.Source = logo;
 		}
+
+		#region - Window Events -
 
 		private void Main_SourceInitialized(object sender, EventArgs e)
 		{
@@ -83,6 +121,30 @@ namespace WCFArchitect.Interface
 				e.Handled = true;
 			}
 		}
+
+		private void CloseWindow_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void MinimizeWindow_Click(object sender, RoutedEventArgs e)
+		{
+			this.WindowState = System.Windows.WindowState.Minimized;
+		}
+
+		private void MaximizeWindow_Click(object sender, RoutedEventArgs e)
+		{
+			this.WindowState = System.Windows.WindowState.Maximized;
+		}
+
+		private void RestoreWindow_Click(object sender, RoutedEventArgs e)
+		{
+			this.WindowState = System.Windows.WindowState.Normal;
+		}
+
+		#endregion
+
+		#region - MessageBox -
 
 		public async void ProcessNextMessage()
 		{
@@ -140,25 +202,9 @@ namespace WCFArchitect.Interface
 				ProcessNextMessage();
 		}
 
-		private void CloseWindow_Click(object sender, RoutedEventArgs e)
-		{
-			this.Close();
-		}
+		#endregion
 
-		private void MinimizeWindow_Click(object sender, RoutedEventArgs e)
-		{
-			this.WindowState = System.Windows.WindowState.Minimized;
-		}
-
-		private void MaximizeWindow_Click(object sender, RoutedEventArgs e)
-		{
-			this.WindowState = System.Windows.WindowState.Maximized;
-		}
-
-		private void RestoreWindow_Click(object sender, RoutedEventArgs e)
-		{
-			this.WindowState = System.Windows.WindowState.Normal;
-		}
+		#region - System Menu -
 
 		private void SystemMenu_Click(object sender, RoutedEventArgs e)
 		{
@@ -182,6 +228,9 @@ namespace WCFArchitect.Interface
 
 		private void SystemMenuOptions_Click(object sender, RoutedEventArgs e)
 		{
+			ActiveProjectScreen.Visibility = System.Windows.Visibility.Collapsed;
+			StartScreen.Visibility = System.Windows.Visibility.Collapsed;
+			OptionsScreen.Visibility = System.Windows.Visibility.Visible;
 		}
 
 		private void SystemMenuHelp_Click(object sender, RoutedEventArgs e)
@@ -192,5 +241,78 @@ namespace WCFArchitect.Interface
 		{
 			this.Close();
 		}
+
+		#endregion
+
+		#region - Options -
+
+
+		private void DefaultProjectFolderBrowse_Click(object sender, RoutedEventArgs e)
+		{
+			string openpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			if (!(Globals.UserProfile.DefaultProjectFolder == "" || Globals.UserProfile.DefaultProjectFolder == null)) openpath = Globals.UserProfile.DefaultProjectFolder;
+
+			Microsoft.WindowsAPICodePack.Dialogs.CommonOpenFileDialog ofd = new Microsoft.WindowsAPICodePack.Dialogs.CommonOpenFileDialog("Select Default Project Directory");
+			ofd.AllowNonFileSystemItems = false;
+			ofd.EnsurePathExists = true;
+			ofd.IsFolderPicker = true;
+			ofd.InitialDirectory = openpath;
+			ofd.Multiselect = false;
+			ofd.ShowPlacesList = true;
+			if (ofd.ShowDialog() == Microsoft.WindowsAPICodePack.Dialogs.CommonFileDialogResult.Cancel) return;
+
+			Globals.UserProfile.DefaultProjectFolder = ofd.FileName;
+			DefaultProjectFolder.Text = Globals.UserProfile.DefaultProjectFolder;
+		}
+
+		private void MILink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+		{
+			string navigateUri = MILink.NavigateUri.ToString();
+			Process.Start(new ProcessStartInfo(navigateUri));
+			e.Handled = true;
+		}
+
+		private void OILLink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+		{
+			string navigateUri = OILLink.NavigateUri.ToString();
+			Process.Start(new ProcessStartInfo(navigateUri));
+			e.Handled = true;
+		}
+
+		private void SLink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+		{
+			string navigateUri = SLink.NavigateUri.ToString();
+			Process.Start(new ProcessStartInfo(navigateUri));
+			e.Handled = true;
+		}
+
+		private void AutomaticBackupsEnabled_Checked(object sender, RoutedEventArgs e)
+		{
+			if (IsLoaded == false) return;
+			AutomaticBackupsEnabled.Content = "Yes";
+			AutomaticBackupsInterval.IsEnabled = true;
+		}
+
+		private void AutomaticBackupsEnabled_Unchecked(object sender, RoutedEventArgs e)
+		{
+			if (IsLoaded == false) return;
+			AutomaticBackupsEnabled.Content = "No";
+			AutomaticBackupsInterval.IsEnabled = false;
+		}
+
+		private void AutomaticBackupsInterval_ValueChanged(object sender, C1.WPF.DateTimeEditors.NullablePropertyChangedEventArgs<TimeSpan> e)
+		{
+			if (IsLoaded == false) return;
+			if (e.NewValue.HasValue == false) { UserProfile.AutomaticBackupsInterval = new TimeSpan(0, 5, 0); return; }
+			if (e.NewValue.Value.TotalMinutes < 1) { UserProfile.AutomaticBackupsInterval = new TimeSpan(0, 1, 0); return; }
+			UserProfile.AutomaticBackupsInterval = e.NewValue.Value;
+			if (Globals.ProjectSpace.State == MP.Karvonite.ObjectSpaceState.Open)
+			{
+				Globals.BackupTimer.Dispose();
+				Globals.BackupTimer = new System.Threading.Timer(new System.Threading.TimerCallback(Globals.BackupSolution), null, (long)UserProfile.AutomaticBackupsInterval.TotalMilliseconds, (long)UserProfile.AutomaticBackupsInterval.TotalMilliseconds);
+			}
+		}
+
+		#endregion
 	}
 }
