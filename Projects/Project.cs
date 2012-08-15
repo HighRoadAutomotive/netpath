82b91ee6274eec8c7f6131cb17085556bc62207f
Adam Wilson <adamw@prospectivesoftware.com>
2012-08-14 19:08:31 -0700

    Cleaned up Project.xaml. Fixed logic in dialogs for Open and Close solution functinos. The Navigator now defaults to the Project screen on load.

--- Projects/Project.cs
+++ Projects/Project.cs
@@ -112,6 +112,9 @@ namespace WCFArchitect.Projects
 		public string ClientOutputFile { get { return (string)GetValue(ClientOutputFileProperty); } set { SetValue(ClientOutputFileProperty, value); } }
 		public static readonly DependencyProperty ClientOutputFileProperty = DependencyProperty.Register("ClientOutputName", typeof(string), typeof(Project), new PropertyMetadata("Client"));
 
+		public bool ServiceSerializeFaults { get { return (bool)GetValue(ServiceSerializeFaultsProperty); } set { SetValue(ServiceSerializeFaultsProperty, value); } }
+		public static readonly DependencyProperty ServiceSerializeFaultsProperty = DependencyProperty.Register("ServiceSerializeFaults", typeof(bool), typeof(Project), new PropertyMetadata(false));
+
 		public ProjectServiceSerializerType ServiceSerializer { get { return (ProjectServiceSerializerType)GetValue(ServiceSerializerProperty); } set { SetValue(ServiceSerializerProperty, value); } }
 		public static readonly DependencyProperty ServiceSerializerProperty = DependencyProperty.Register("ServiceSerializer", typeof(ProjectServiceSerializerType), typeof(Project));
 
