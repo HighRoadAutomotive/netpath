840666cedcc6dbe0303a22f7fb2a71e19b802aeb
Adam Wilson <adamw@prospectivesoftware.com>
2012-11-11 17:45:56 -0800

    Added the WinRT Toolkit project.

---
+++ ToolkitWinRT/DependencyObjectEx.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+namespace WCFArchitect
+{
+	namespace Toolkit
+	{
+		namespace WinRT
+		{
+			
+			public ref class DependencyObjectEx : Windows::UI::Xaml::DependencyObject
+			{
+			internal:
+				DependencyObjectEx();
+
+			public:
+				Object^ GetValueThreaded(Windows::UI::Xaml::DependencyProperty^ dp);
+				void SetValueThreaded(Windows::UI::Xaml::DependencyProperty^ dp, Object^ value);
+			};
+		}
+	}
+}
