5b0658d8651520216e9f8e0a97ef50e21cb4f6f5
Adam Wilson <adamw@prospectivesoftware.com>
2014-01-07 18:08:54 -0800

    Build script fixes.

--- Build.sh
+++ Build.sh
@@ -4,9 +4,9 @@ git submodule update
 
 #Build System.Utilities
 cd System.Utilities
-git pull origin net.randombit.botan
-/C/Windows/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe System.Utilities.sln -nologo -p:Configuration=Release -t:Clean\;Build -p:TrackFileAccess=false
+git pull origin master
+"/C/Program Files (x86)/MSBuild/12.0/Bin/MSBuild.exe" System.Utilities.sln -nologo -p:Configuration=Release -t:Clean\;Build -p:TrackFileAccess=false
 cd ..
 
 #Build the NETPath SDK
-/C/Windows/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe SDK.sln -nologo -p:Configuration=Release -t:Clean\;Build -p:TrackFileAccess=false
+"/C/Program Files (x86)/MSBuild/12.0/Bin/MSBuild.exe" SDK.sln -nologo -p:Configuration=Release -t:Clean\;Build -p:TrackFileAccess=false
