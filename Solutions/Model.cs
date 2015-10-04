1f5da253faf86bf808989444f22bc14921cb63c7
Adam Wilson <adamw@ellipticbit.com>
2015-10-03 18:14:48 -0700

    Organzied directories. Built Solution object model.

---
+++ Solutions/Model.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestForge.Solutions
+{
+	[JsonObject(MemberSerialization.OptIn)]
+	public sealed class Solution
+	{
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("solutionVersion")]
+		public string SolutionVersion { get; internal set; }
+
+		[JsonProperty("minimumVersion")]
+		public string MinimumVersion { get; internal set; }
+
+		[JsonProperty("subfolders")]
+		public List<SolutionFolder> Folders { get; private set; }
+
+		[JsonProperty("projects")]
+		public List<Project> Projects { get; private set; }
+
+		public Solution()
+		{
+			Folders = new List<SolutionFolder>();
+			Projects = new List<Project>();
+		}
+	}
+
+	[JsonObject(MemberSerialization.OptIn)]
+	public sealed class SolutionFolder
+	{
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("subfolders")]
+		public List<SolutionFolder> Folders { get; private set; }
+
+		[JsonProperty("projects")]
+		public List<Project> Projects { get; private set; }
+
+		public SolutionFolder()
+		{
+			Folders = new List<SolutionFolder>();
+			Projects = new List<Project>();
+		}
+	}
+
+	[JsonObject(MemberSerialization.OptIn)]
+	public sealed class Project
+	{
+		[JsonProperty("id")]
+		public Guid ID { get; private set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("path")]
+		public string Path { get; set; }
+
+		public Project(Guid ProjectID)
+		{
+			ID = ProjectID;
+		}
+	}
+}
---
+++ Solutions/Storage.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestForge.Solutions
+{
+	public static class Storage
+	{
+		public static async Task<Solution> Open(string path)
+		{
+			using(var fs = File.OpenRead(path))
+			using (var sr = new StreamReader(fs))
+			{
+				var json = await sr.ReadToEndAsync();
+				return await Task.Run(() => JsonConvert.DeserializeObject<Solution>(json));
+			}
+		}
+
+		public static async Task Save(string path, Solution solution)
+		{
+			var json = await Task.Run(() => JsonConvert.SerializeObject(solution));
+			using (var fs = File.Open(path, FileMode.OpenOrCreate))
+			using (var sw = new StreamWriter(fs))
+			{
+				await sw.WriteAsync(json);
+				await sw.FlushAsync();
+				sw.Close();
+			}
+		}
+	}
+}
