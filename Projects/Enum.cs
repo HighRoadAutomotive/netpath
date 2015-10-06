785ba065592325e47f683c063e46f780db3f6bad
Adam Wilson <adamw@ellipticbit.com>
2015-10-05 20:58:21 -0700

    Forgot to add files.

---
+++ Projects/Enum.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestForge.Projects
+{
+	public interface IEnum
+	{
+		string Name { get; }
+		bool Collapsed { get; }
+		List<IEnumElement> IElements { get; }
+	}
+
+	public interface IEnumElement
+	{
+		string Name { get; }
+		bool IsExcluded { get; }
+		bool IsHidden { get; }
+		bool IsAuto { get; }
+		bool IsCustom { get; }
+		bool IsAggregate { get; }
+		long ServerValue { get; }
+		long? ClientValue { get; }
+		List<IEnumElement> IAggregateValues { get; }
+	}
+
+	[JsonObject(MemberSerialization.OptIn)]
+	public abstract class EnumBase<P, N, E> : IEnum where P : ProjectBase<N> where N : NamespaceBase<ProjectBase<N>, N>
+	{
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("collapsed")]
+		public bool Collapsed { get; set; }
+
+		[JsonProperty("project", ItemIsReference = true)]
+		public P Project { get; private set; }
+
+		[JsonProperty("parent", ItemIsReference = true)]
+		public N Parent { get; private set; }
+
+		[JsonProperty("children")]
+		public List<E> Elements { get; private set; }
+
+		[JsonIgnore]
+		List<IEnumElement> IEnum.IElements { get { return  (List<IEnumElement>) Elements.AsEnumerable(); } }
+	}
+
+	[JsonObject(MemberSerialization.OptIn)]
+	public abstract class EnumElementBase<P, N, E> : IEnumElement where E : EnumBase<P, N, EnumElementBase<P, N, E>> where P : ProjectBase<N> where N : NamespaceBase<ProjectBase<N>, N>
+	{
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("parent", ItemIsReference = true)]
+		public P Parent { get; private set; }
+
+		[JsonProperty("excluded")]
+		public bool IsExcluded { get; set; }
+
+		[JsonProperty("hidden")]
+		public bool IsHidden { get; set; }
+
+		[JsonProperty("auto")]
+		public bool IsAuto { get; set; }
+
+		[JsonProperty("custom")]
+		public bool IsCustom { get; set; }
+
+		[JsonProperty("aggregate")]
+		public bool IsAggregate { get; set; }
+
+		[JsonProperty("servervalue")]
+		public long ServerValue { get; set; }
+
+		[JsonProperty("clientvalue")]
+		public long? ClientValue { get; set; }
+
+		[JsonProperty("aggregates", ItemIsReference = true)]
+		public List<E> AggregateValues { get; private set; }
+
+		[JsonIgnore]
+		List<IEnumElement> IEnumElement.IAggregateValues { get { return (List<IEnumElement>)AggregateValues.AsEnumerable(); } }
+
+		public EnumElementBase(P parent, string name)
+		{
+			Name = name;
+			Parent = parent;
+		}
+	}
+}
---
+++ Projects/Project.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace RestForge.Projects
+{
+	public interface IProject
+	{
+		Guid ID { get; }
+		string Name { get; }
+		string SolutionPath { get; }
+		List<Guid> Dependencies { get; }
+	}
+
+	[JsonObject(MemberSerialization.OptIn)]
+	public abstract class ProjectBase<N> where N : NamespaceBase<ProjectBase<N>, N>
+	{
+		[JsonProperty("id")]
+		public Guid ID { get; private set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("solutionPath")]
+		public string SolutionPath { get; set; }
+
+		[JsonProperty("dependencies")]
+		public List<Guid> Dependencies { get; private set; }
+
+		[JsonProperty("root")]
+		public N Root { get; private set; }
+
+		protected ProjectBase()
+		{
+			ID = Guid.NewGuid();
+			Dependencies = new List<Guid>();
+		}
+
+		public abstract Task Build();
+		public abstract Task Save();
+	}
+
+	public interface INamespace
+	{
+		string Name { get; }
+		bool Collapsed { get; }
+		List<INamespace> IChildren { get; }
+	}
+
+	[JsonObject(MemberSerialization.OptIn)]
+	public abstract class NamespaceBase<P, N> : INamespace where P : ProjectBase<N> where N : NamespaceBase<ProjectBase<N>, N>
+	{
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("collapsed")]
+		public bool Collapsed { get; set; }
+
+		[JsonProperty("project", ItemIsReference = true)]
+		public P Project { get; private set; }
+
+		[JsonProperty("parent", ItemIsReference = true)]
+		public N Parent { get; private set; }
+
+		[JsonProperty("children")]
+		public List<N> Children { get; private set; }
+
+		[JsonIgnore]
+		List<INamespace> INamespace.IChildren { get { return (List<INamespace>)Children.AsEnumerable(); } }
+
+
+		[JsonIgnore]
+		public string FullNamespace { get { return GetFullNamespace(); } }
+
+		protected NamespaceBase(P project, N parent)
+		{
+			Children = new List<N>();
+			Project = project;
+			Parent = parent;
+		}
+
+		protected abstract string GetFullNamespace();
+	}
+}
