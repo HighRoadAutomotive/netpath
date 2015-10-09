c3b3c0376cbafd7cd258fe77f0d54f45a7bee01d
Adam Wilson <adamw@ellipticbit.com>
2015-10-08 23:27:31 -0700

    Updataed build scripts.

--- Build.sh
+++ Build.sh
@@ -7,15 +7,13 @@ git submodule update
 
 #Build System.Utilities
 cd System.Utilities
-git pull origin master
 "/C/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe" ./WinRT8/WinRT8.csproj -nologo -p:Configuration=Release -p:Platform=AnyCPU -t:Build
 "/C/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe" ./NET45/NET45.csproj -nologo -p:Configuration=Release -p:Platform=AnyCPU -t:Build
 cd ..
 
 cd EllipticBit.Controls
-git pull origin master
 sh BuildVS14.sh
 cd ..
 
-#Build NETPath
-"/C/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe" ./NETPath.sln -nologo -t:Build
+#Build RestForge
+#"/C/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe" ./RestForge.sln -nologo -t:Build
