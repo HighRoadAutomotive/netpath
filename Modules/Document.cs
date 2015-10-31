9cc67342e068cb2a0ef37a0b24dc929e1c7e3b7a
Adam Wilson <adamw@ellipticbit.com>
2015-10-30 21:11:25 -0700

    Started working on Module interfaces.

---
+++ Modules/Document.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RestForge.Modules
+{
+	public abstract class Document : ContentControl
+	{
+		public Guid ID { get; private set; }
+
+		public string Title { get { return (string)GetValue(TitleProperty); } protected set { SetValue(TitlePropertyKey, value); } }
+		private static readonly DependencyPropertyKey TitlePropertyKey = DependencyProperty.RegisterReadOnly("Title", typeof(string), typeof(Document), new PropertyMetadata(""));
+		public static readonly DependencyProperty TitleProperty = TitlePropertyKey.DependencyProperty;
+
+		public bool CanClose { get { return (bool)GetValue(CanCloseProperty); } protected set { SetValue(CanClosePropertyKey, value); } }
+		private static readonly DependencyPropertyKey CanClosePropertyKey = DependencyProperty.RegisterReadOnly("CanClose", typeof(bool), typeof(Document), new PropertyMetadata(true));
+		public static readonly DependencyProperty CanCloseProperty = CanClosePropertyKey.DependencyProperty;
+
+		public bool CanPin { get { return (bool)GetValue(CanPinProperty); } protected set { SetValue(CanPinPropertyKey, value); } }
+		private static readonly DependencyPropertyKey CanPinPropertyKey = DependencyProperty.RegisterReadOnly("CanPin", typeof(bool), typeof(Document), new PropertyMetadata(false));
+		public static readonly DependencyProperty CanPinProperty = CanPinPropertyKey.DependencyProperty;
+
+		private object _content = null;			//This is used to store the visual tree when not displayed.
+
+		public void EnableContentRendering()
+		{
+			this.Content = _content;
+			_content = null;
+		}
+
+		public void DisableContentRendering()
+		{
+			_content = this.Content;
+			this.Content = null;
+		}
+	}
+}
---
+++ Modules/Module.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestForge.Modules
+{
+	public abstract class Module
+	{
+		public string Name { get; protected set; }
+	}
+}
